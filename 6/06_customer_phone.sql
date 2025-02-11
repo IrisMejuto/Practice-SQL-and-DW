d04b852a010143d9aac34ad5702fd88ef58e7c09
Iris Mejuto <121298708+IrisMejuto@users.noreply.github.com>
2025-02-11 00:24:34 +0000

    Create 06_customer_phone.sql

---
+++ 6/06_customer_phone.sql
@@ -0,0 +1,20 @@
+WITH customer_phone_update AS (
+  SELECT 
+    CAST(calls_ivr_id AS STRING) as calls_ivr_id,
+    customer_phone,
+    row_number()over(
+    partition by cast (calls_ivr_id as string) order by 
+        case 
+          when customer_phone != 'UNKNOWN' then 1
+          else 2 
+        end,
+        customer_phone
+    ) as rn
+  FROM `keepcoding.ivr_detail`
+)
+SELECT 
+  calls_ivr_id,
+  customer_phone
+FROM customer_phone_update
+WHERE rn = 1
+;
