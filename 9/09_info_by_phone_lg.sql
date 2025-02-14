6ddc990dc15c1a3911257e733596defd6c002ffd
Iris Mejuto <121298708+IrisMejuto@users.noreply.github.com>
2025-02-14 21:43:02 +0000

    Create 09_info_by_phone_lg.sql

---
+++ 9/09_info_by_phone_lg.sql
@@ -0,0 +1,9 @@
+SELECT
+  calls_ivr_id,
+  CASE
+    WHEN step_name = 'CUSTOMERINFOBYPHONE.TX' and step_result = 'OK' THEN 1
+    ELSE 0
+  END AS  info_by_phone_lg
+FROM `keepcoding.ivr_detail`
+ORDER BY 2 DESC
+;
