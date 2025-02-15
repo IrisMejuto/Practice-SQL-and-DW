7301f5791d04feb72597b662caa8471993f8702d
Iris Mejuto <121298708+IrisMejuto@users.noreply.github.com>
2025-02-15 15:33:02 +0000

    Update 07_ billing_account_id.sql

--- 07/07_ billing_account_id.sql
+++ 07/07_ billing_account_id.sql
@@ -1,8 +1,8 @@
 SELECT 
-  CAST(calls_ivr_id AS STRING) as calls_ivr_id,
+  calls_ivr_id,
   billing_account_id
 FROM `keepcoding.ivr_detail`
 WHERE billing_account_id != 'UNKNOWN'
 AND billing_account_id IS NOT NULL
-qualify row_number() over(partition by calls_ivr_id order by billing_account_id desc) = 1
+qualify row_number() over(partition by CAST(calls_ivr_id AS STRING) order by billing_account_id desc) = 1
 ;
