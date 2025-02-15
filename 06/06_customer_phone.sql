d1d45255bfb4a2a40d0e8f6fb4cf6f41dd2c677b
Iris Mejuto <121298708+IrisMejuto@users.noreply.github.com>
2025-02-15 15:32:04 +0000

    Update 06_customer_phone.sql

--- 06/06_customer_phone.sql
+++ 06/06_customer_phone.sql
@@ -1,7 +1,7 @@
 SELECT
-  cast(calls_ivr_id as string) as calls_ivr_id,
+  calls_ivr_id,
   customer_phone
 FROM `keepcoding.ivr_detail`
 WHERE customer_phone != 'UNKNOWN'
-qualify row_number() over(partition by calls_ivr_id order by customer_phone desc) = 1
+qualify row_number() over(partition by cast(calls_ivr_id as string) order by customer_phone desc) = 1
 ;
