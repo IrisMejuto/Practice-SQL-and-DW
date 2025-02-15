027e07df1bbbe4f1de64a1da65c1cf7fcb39c33b
Iris Mejuto <121298708+IrisMejuto@users.noreply.github.com>
2025-02-15 15:31:07 +0000

    Update 05_document_type_and_document_identification.sql

--- 05/05_document_type_and_document_identification.sql
+++ 05/05_document_type_and_document_identification.sql
@@ -1,8 +1,8 @@
 SELECT
-  cast(calls_ivr_id as string) as calls_ivr_id,
+  calls_ivr_id,
   document_type,
   document_identification
 FROM `keepcoding.ivr_detail`
 WHERE document_type IN ('DNI', 'NIE', 'CIF', 'PASSPORT')
-qualify row_number() over(partition by calls_ivr_id order by document_type desc) = 1
+qualify row_number() over(partition by cast(calls_ivr_id as string) order by document_type desc) = 1
 ;
