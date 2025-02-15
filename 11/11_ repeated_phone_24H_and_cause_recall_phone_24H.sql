0cee338662c31d8bf05bef940a7c0e2c3aa71e21
Iris Mejuto <121298708+IrisMejuto@users.noreply.github.com>
2025-02-15 15:41:47 +0000

    Update 11_ repeated_phone_24H_and_cause_recall_phone_24H.sql

--- 11/11_ repeated_phone_24H_and_cause_recall_phone_24H.sql
+++ 11/11_ repeated_phone_24H_and_cause_recall_phone_24H.sql
@@ -3,13 +3,12 @@ SELECT
   calls_phone_number,
   calls_start_date,
   CASE
-    WHEN DATETIME_DIFF(calls_start_date,LAG(calls_start_date) over(partition by calls_phone_number order by calls_start_date),hour) <=24 THEN 1
+    WHEN DATETIME_DIFF(calls_start_date,LAG(calls_start_date) over(partition by cast(calls_phone_number as string) order by calls_start_date),hour) <=24 THEN 1
     ELSE 0
   END AS repeated_phone_24H,
   CASE
-    WHEN DATETIME_DIFF(LEAD(calls_start_date) OVER(partition by calls_phone_number order by calls_start_date), calls_start_date, hour) <=24 THEN 1
+    WHEN DATETIME_DIFF(LEAD(calls_start_date) OVER(partition by cast(calls_phone_number as string) order by calls_start_date), calls_start_date, hour) <=24 THEN 1
     ELSE 0
   END AS cause_recall_phone_24H
 FROM `keepcoding.ivr_detail`
 ORDER BY 3 ASC
-;
